2b2bc8a77a1718080d0f94ef9ef6834a74c8d455
SaintHazzard <chamitos1998@hotmail.com>
2024-02-15 07:33:25 -0500

    feat: :sparkles: Funcion calcular
    Se agrega el evento calcular a todos los botone de monedas

--- storage/js/maquinaExpWeb.js
+++ storage/js/maquinaExpWeb.js
@@ -2,6 +2,9 @@
 
 const contenido = document.querySelector(".contenido")
 const modal = document.querySelector(".modal")
+const modalContent = document.querySelector("#modalContent")
+let precioProducto;
+let nodo;
 /**
  * @typedef {Object} producto
  * @property {string} nombre
@@ -17,7 +20,7 @@ const productos = [
   { nombre: 'Producto C', precio: 390, img: "/storage/images/R1/R1-2022-COLOR_AZUL.jpg" },
 ];
 const monedas = [10, 50, 100]
-let inMonedas = 0
+let inMonedas = 0;
 
 /**
  * @param {Array<producto>} productos
@@ -26,7 +29,6 @@ export function crearCards(productos) {
   productos.forEach((producto) => {
     let card = document.createElement('div');
     card.classList.add('cardItem');
-    producto
     let productName = document.createElement('h2');
     productName.textContent = producto.nombre
     let productPrice = document.createElement('p');
@@ -37,8 +39,9 @@ export function crearCards(productos) {
     button.textContent = "Comprar"
     button.classList.add("botonAzul")
     button.addEventListener('click', function () {
-      return producto.precio
+      precioProducto = producto.precio
     })
+
 
     card.appendChild(imgProduct);
     card.appendChild(productName);
@@ -53,14 +56,29 @@ export function crearCards(productos) {
  * @param {Array} monedas
  * @param {number} coin 
  */
-export function calcular(coin) {
+export function calcular(precioProducto, coin) {
   inMonedas += coin
-  console.log(inMonedas);
-  // if (inMonedas < ) {
+  let vueltos = inMonedas - precioProducto
+  if (inMonedas >= precioProducto) {
+    modalContent.innerHTML += `Su vuelto es: <br/> ${vueltos} <br/> Monedas: <br/>`
+    while (Math.floor(vueltos / 100) > 0) {
+      modalContent.innerHTML += `$ 100 <br/>`
+      vueltos -= 100
+    }
+    while (Math.floor(vueltos / 50) > 0) {
+      modalContent.innerHTML += `$ 50 <br/>`
+      vueltos -= 50
+    }
+    while (Math.floor(vueltos / 10) > 0) {
+      modalContent.innerHTML += `$ 10 <br/>`
+      vueltos -= 10
+    }
+    inMonedas = 0
+    vueltos = 0
+  }
 
-  // }
 }
-
+let button = document.querySelector('button')
 
 /**
  * Maneja el evento de clic.
@@ -68,19 +86,23 @@ export function calcular(coin) {
  */
 contenido.addEventListener("click", (event) => {
   const target = event.target
-  const modalContent = document.querySelector("#modalContent")
+
   let items = document.querySelectorAll(".botonAzul")
   if (target.classList.contains('botonAzul')) {
     modal.classList.toggle('modalVisible')
 
   }
+  let h2 = document.createElement('h2')
+  h2.textContent = "Las monedas que puede ingresar son: "
+  modalContent.appendChild(h2)
   monedas.forEach(coin => {
     let btn = document.createElement("button")
     btn.classList.add('botonAzul')
     btn.textContent = `${coin}`
-    btn.addEventListener("click", function () {
-      calcular(coin,)
+    btn.addEventListener('click', () => {
+      calcular(precioProducto, coin)
     })
+
     modalContent.appendChild(btn)
   })
 })
@@ -90,3 +112,9 @@ contenido.addEventListener("click", (event) => {
 document.addEventListener("DOMContentLoaded", () => {
   crearCards(productos)
 })
+
+const closeBtn = document.getElementById('closeBtn');
+closeBtn.addEventListener('click', function () {
+  modalContent.innerHTML = ``
+  modal.classList.toggle('modalVisible')
+});
